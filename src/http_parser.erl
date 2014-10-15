86dd79c992c261ae0dc75decfb2ab83b0cd506df
kristian@purestyle.se <kristian@purestyle.se>
2014-10-15 21:14:20 +0200

    added dialyzer specs

--- src/http_parser.erl
+++ src/http_parser.erl
@@ -1,35 +1,40 @@
 -module(http_parser).
 -export([parse_request/1, response/1]).
 
+-type http_info() :: {atom(), string(), [{atom(), atom()}], atom()}.
+
 % Parse a request and return the
 % request, headers and body as a tuple
+-spec parse_request(string()) -> {http_info(),
+                                  {[string()], string()},
+                                  string()}.
 parse_request(R0) ->
-    {Request, R1} = request_line(R0),
-    {Headers, R2} = headers(R1),
-    {Body,    _ } = message_body(R2),
-    {Request, Headers, Body}.
+    {HttpInfo, R1}  = request_line(R0),
+    {Headers, Body} = headers(R1),
+    {HttpInfo, Headers, Body}.
 
+-spec request_line(string()) -> {http_info(), string()}.
 request_line([$G, $E, $T, 32 | R0]) ->
     line_continue(get, R0);
-
 request_line([$P, $U, $T, 32 | R0]) ->
     line_continue(put, R0);
-
 request_line([$P, $O, $S, $T, 32 | R0]) ->
     line_continue(post, R0);
-
 request_line([$D, $E, $L, $E, $T, $E, 32 | R0]) ->
     line_continue(delete, R0).
 
+-spec throw_params(string()) -> string().
 throw_params(URI) ->
     lists:takewhile(fun(X) -> X /= $? end, URI).
 
+-spec keep_params(string()) -> [{atom(), atom()}].
 keep_params(URI) ->
     case lists:dropwhile(fun(X) -> X /= $? end, URI) of
         [] -> []; %no GET parameters encountered
         Ls -> parameters(lists:nthtail(1, Ls)) % first, remove the ? char
     end.
 
+-spec line_continue(atom(), string()) -> {http_info(), string()}.
 line_continue(Method, R0) ->
     {URI, R1}     = request_uri(R0),
     {Ver, R2}     = http_version(R1),
@@ -37,6 +42,7 @@ line_continue(Method, R0) ->
     {{Method, throw_params(URI), keep_params(URI), Ver}, R3}.
 
 % 32 is the last byte of the request uri (space), R0 is the last rest
+-spec request_uri(string()) -> {[string()], string()}.
 request_uri([32|R0]) ->
     {[], R0};
 % intil 32 is found, keep adding the head element
@@ -45,6 +51,7 @@ request_uri([C|R0]) ->
     {Rest, R1} = request_uri(R0),
     {[C | Rest], R1}.
 
+-spec http_version(string()) -> {atom(), string()}.
 http_version([$H, $T, $T, $P, $/, $1, $., $1 | R0]) ->
     {v11, R0};
 http_version([$H, $T, $T, $P, $/, $1, $., $0 | R0]) ->
@@ -52,6 +59,7 @@ http_version([$H, $T, $T, $P, $/, $1, $., $0 | R0]) ->
 
 % recursively pick out all headers until
 % the stop "[13,10]" CRLF
+-spec headers(string()) -> {[string()], string()}.
 headers([13, 10 | R0]) ->
     {[],R0};
 headers(R0) ->
@@ -62,15 +70,14 @@ headers(R0) ->
 % take everything until [13,10]
 % return that, and the rest of the list
 % recursively pick out one header CRLF
+-spec header(string()) -> {string(), string()}.
 header([13, 10 | R0]) ->
     {[], R0};
 header([C|R0]) ->
     {Rest, R1} = header(R0),
     {[C|Rest], R1}.
 
-message_body(R) ->
-    {R, []}.
-
+-spec parameters(string()) -> [{atom(), atom()}].
 parameters(Ls) ->
     {Parameter, Rest} = parameter(Ls),
     case Rest of
@@ -78,6 +85,7 @@ parameters(Ls) ->
         _  -> [Parameter] ++ parameters(lists:nthtail(1,Rest))
     end.
 
+-spec parameter(string()) -> {{atom(), atom()}, string()}.
 parameter(Ls) ->
     {Name, Rest}     = lists:splitwith(fun(X) -> X /= $= end, Ls),
     {Content, Rest2} = lists:splitwith(fun(X) -> X /= $& end,
@@ -86,6 +94,7 @@ parameter(Ls) ->
     {{Name, Content}, Rest2}.
 
 %% for now always send access-control-allow
+-spec response(string()) -> string().
 response(Body) ->
     "HTTP/1.1 200 OK\r\n" ++
     "Access-Control-Allow-Origin: *\r\n"++ "\r\n" ++ Body.
