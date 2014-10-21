656067c193fd7f933638e0e14c2e7c593295e253
kristian@purestyle.se <kristian@purestyle.se>
2014-10-21 13:22:37 +0200

    included SASL in the start script

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-erl -noshell -sname rest_server -setcookie adm -pa ebin -eval "commands:start()" &
+erl -noshell -sname rest_server -setcookie adm -boot start_sasl -pa ebin -eval "commands:start()" &
