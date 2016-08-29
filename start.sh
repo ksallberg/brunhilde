a763fe86da1af2e1522fedd8cf22a582db3533a1
kristian@purestyle.se <kristian@purestyle.se>
2016-08-29 23:37:43 +0200

    Stable version of jsx

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-erlc -pa -pa _build/default/lib/*/ebin -o priv/ servers/*.erl
+erlc -pa _build/default/lib/*/ebin -o priv/ servers/*.erl
 erl -boot start_sasl -pa _build/default/lib/*/ebin \
     -pa priv -eval "application:start(rest_server)" \
     -config ebin/sys.config
